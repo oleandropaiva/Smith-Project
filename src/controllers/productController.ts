fa9f665cc958ba0543495562534981e5b0322974
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-06 10:47:50 -0300

    desenv rotas req1

---
+++ src/controllers/productController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import productService from '../services/productService';
+
+const productController = {
+  async addProducts(req:Request, res:Response) {
+    const { name, amount } = req.body;
+    const { code, data } = await productService.addProducts(name, amount);
+    return res.status(code).json(data);
+  },
+};
+
+export default productController;
